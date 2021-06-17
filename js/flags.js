7eb824bbf3f9378d157fa890fb8e5bec694bec0a
Luis <admin@luiscaldas.com>
2021-06-17 23:03:58 +0100

    added a js file with the flags names

---
+++ js/flags.js
@@ -0,0 +1,30 @@
+/*********
+ * Flags *
+ *********/
+
+'use strict';
+
+/***********
+ * Globals *
+ ***********/
+
+const flags = [
+	"alpha"   , "bravo"  , "charlie" , "delta"    , "echo"    ,
+	"foxtrot" , "golf"   , "hotel"   , "india"    , "juliett" ,
+	"kilo"    , "lima"   , "mike"    , "november" , "oscar"   ,
+	"papa"    , "quebec" , "romeo"   , "sierra"   , "tango"   ,
+	"uniform" , "victor" , "whiskey" , "x-ray"    , "yankee"  ,
+	"zulu"    ,
+	"one"     , "two"    , "three"   , "four"     , "five"    ,
+	"six"     , "seven"  , "eight"   , "niner"    , "zero"    , 
+	"pennant_one"   , "pennant_two"   , "pennant_three" ,
+	"pennant_four"  , "pennant_five"  , "pennant_six"   ,
+	"pennant_seven" , "pennant_eight" , "pennant_niner" ,
+	"pennant_zero"  ,
+	"answer"      , "corpen"    , "designation" , "division" ,
+	"emergency"   , "formation" , "group"       , "negative" ,
+	"port"        , "prepare"   , "question"    , "screen"   ,
+	"speed"       , "squadron"  , "starboard"   , "station"  ,
+	"subdivision" , "turn"      , 
+	"sub_one"     , "sub_two"   , "sub_three"   , "sub_four"
+]
